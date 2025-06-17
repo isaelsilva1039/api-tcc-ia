9de8bc34442a98b5544390a132a83e039edb1f3b
isaelsilva1039@ <isaelsilva1039@gmail.com>
2025-06-17 19:04:18 -0300

    Api com GPT para buscar

---
+++ src/main/java/java_core_api/api_java_core/services/GptService.java
@@ -0,0 +1,19 @@
+package java_core_api.api_java_core.services;
+
+import java_core_api.api_java_core.dtos.FabricanteDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GptService {
+
+    public String listaPosts(FabricanteDTO fabricanteDTO) {
+        return "sss";
+    }
+
+
+
+
+}
